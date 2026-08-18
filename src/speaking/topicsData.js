9871bedc4924f94ab286040b8e03fe06b0132522
djurabekmirzaev82-afk <djurabekmirzaev82@gmail.com>
2026-08-18 16:52:07 +0500

    Add speaking topics data structure

--- src/speaking/topicsData.js
+++ src/speaking/topicsData.js
@@ -1,1 +1,13 @@
-
+export const speakingTopics = [
+  {
+    id: 1,
+    title: "Family and Friends",
+    icon: "👨‍👩‍👧‍👦",
+    vocabulary: [
+      { word: "close-knit family", definition: "A close family with common interests" },
+      { word: "hit it off", definition: "To like each other straight away" },
+    ],
+    questions: ["Do you come from a large family?"],
+    sampleAnswer: "My immediate family is not very big..."
+  },
+];
