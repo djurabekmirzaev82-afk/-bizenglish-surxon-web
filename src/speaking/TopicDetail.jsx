e925247f3cade77364eba830d09876c2f6093d9e
djurabekmirzaev82-afk <djurabekmirzaev82@gmail.com>
2026-08-18 16:51:13 +0500

    Add TopicDetail component for displaying topic details

--- src/speaking/TopicDetail.jsx
+++ src/speaking/TopicDetail.jsx
@@ -1,1 +1,15 @@
+import React from 'react';
+import { useParams, Link } from 'react-router-dom';
 
+const TopicDetail = () => {
+  const { id } = useParams();
+  
+  return (
+    <div>
+      <h1>📖 Mavzu #{id}</h1>
+      <Link to="/speaking">⬅️ Barcha mavzular</Link>
+    </div>
+  );
+};
+
+export default TopicDetail;
