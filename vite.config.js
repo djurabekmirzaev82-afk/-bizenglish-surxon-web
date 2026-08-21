31b97a139a30de7c984fb3b07e4d845f6c3fb84d
djurabekmirzaev82-afk <djurabekmirzaev82@gmail.com>
2026-08-21 19:06:02 +0500

    Configure build options and chunking for Vite

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,17 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+
+  build: {
+    chunkSizeWarningLimit: 500,
+
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ["react", "react-dom"],
+          charts: ["recharts"],
+        },
+      },
+    },
+  },
 });
